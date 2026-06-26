c287266be796807597ba9fad7e6f15ee45d67327
Streetsraker <kim842058@gmail.com>
2026-06-26 23:36:23 +0300

    [FIX] init_db

--- backend/src/init_db.py
+++ backend/src/init_db.py
@@ -4,10 +4,13 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from backend.src import models
-from backend.src.database import SessionLocal
+from backend.src.database import SessionLocal, engine, Base
 from backend.src.services.auth import hash_password
 
+
 def create_initial_admin():
+    Base.metadata.create_all(bind=engine)
+
     db = SessionLocal()
     try:
         admin_exists = db.query(models.User).filter(models.User.username == "admin").first()
@@ -28,6 +31,7 @@ def create_initial_admin():
         print(f"Ошибка при инициализации базы данных: {e}")
     finally:
         db.close()
+
 
 if __name__ == "__main__":
     create_initial_admin()
